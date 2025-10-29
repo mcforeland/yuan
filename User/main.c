30593412c51516e8c371cfe0381c477cfc8ef18e
yzh <499807347@qq.com>
2025-10-29 16:06:15 +0800

    feat:修改2

--- User/main.c
+++ User/main.c
@@ -32,6 +32,8 @@ void vl53l0x_gpio_Init(void)
 	GPIO_InitTypeDef GPIO_InitStructure;
 	
 	
+
+	
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
